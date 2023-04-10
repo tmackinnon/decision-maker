5a2c11aa7182e77debe61c88cabe6efeef5cb13b
Omar Hamid <omarhamid664@gmail.com>
2023-04-10 12:59:31 -0600

    Edit seeds

--- db/seeds/polls.sql
+++ db/seeds/polls.sql
@@ -5,15 +5,15 @@ INSERT INTO options (poll_id, title, description) VALUES (1, 'The Hobbit', 'FANT
 INSERT INTO options (poll_id, title, description) VALUES (1, 'The Truman Show', 'COMEDY');
 INSERT INTO options (poll_id, title, description) VALUES (1, 'Iron Man', 'Action movie');
 
-INSERT INTO options (poll_id, title, description) VALUES (2, 'Goodfellas', 'Mafia movie');
-INSERT INTO options (poll_id, title, description) VALUES (2, 'No country for old men', 'Detective');
-INSERT INTO options (poll_id, title, description) VALUES (2, 'The Shawshank Redemption', 'Drama');
+INSERT INTO options (poll_id, title, description) VALUES (2, 'Gangsta''s Paradise', 'Coolio');
+INSERT INTO options (poll_id, title, description) VALUES (2, 'Waterfalls', 'TLC');
+INSERT INTO options (poll_id, title, description) VALUES (2, 'Fantasy', 'Mariah Carey');
 
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (1, 'Alex', 1);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (2, 'Alex', 3);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (3, 'Alex', 2);
 
-INSERT INTO rankings (option_id, voter_name, rank) VALUES (1, 'Jong', 3);
+INSERT INTO rankings (option_id, voter_name, rank) VALUES (1, 'John', 3);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (2, 'John', 2);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (3, 'John', 1);
 
@@ -25,7 +25,7 @@ INSERT INTO rankings (option_id, voter_name, rank) VALUES (4, 'Alex', 1);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (5, 'Alex', 3);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (6, 'Alex', 2);
 
-INSERT INTO rankings (option_id, voter_name, rank) VALUES (4, 'Jong', 3);
+INSERT INTO rankings (option_id, voter_name, rank) VALUES (4, 'John', 3);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (5, 'John', 2);
 INSERT INTO rankings (option_id, voter_name, rank) VALUES (6, 'John', 1);
 
