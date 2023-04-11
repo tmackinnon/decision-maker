f0f822f4fb2db6823a5d349f1c86b1e388a2b5c8
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-11 00:22:43 +0000

    add query to pull out option and poll data

--- db/queries/vote.js
+++ db/queries/vote.js
@@ -1,9 +1,10 @@
+const { query } = require('express');
 const db = require('../connection');
 
 //example
 const getPollInfo = (id) => {
   const queryString = `
-  SELECT options.title as oTitle, options.description as oDesc, options.id, polls.title as pTitle, polls.description as pDesc
+  SELECT options.title as otitle, options.description as odesc, options.id, polls.title as ptitle, polls.description as pdesc
   FROM options
   JOIN polls ON polls.id = poll_id
   WHERE polls.id = $1;`
@@ -14,6 +15,28 @@ const getPollInfo = (id) => {
       return data.rows;
     });
 };
+
+//req.body= { sortedIds: [ '2', '1', '3' ], voter: 'Tara' }
+
+// const submitVotes = (object) => {
+//   const voter_name = object.voter;
+//   const optionIds = object.sortedIds; //[ '2', '1', '3' ]
+//   for (const options of optionIds) {
+
+//   }
+//   const queryParams = [];
+//   const queryString = `
+//   INSERT INTO rankings (option_id, voter_name, rank)
+//   VALUES ($1, $2, $3)
+//   RETURNING *;
+//   `;
+
+//   return db
+//     .query(queryString, queryParams)
+//     .then()
+
+// }
+
 
 module.exports = { getPollInfo };
 
