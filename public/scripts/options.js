0bfbdc20b870e4b1f27fdb4b18b862538c511a15
Omar Hamid <omarhamid664@gmail.com>
2023-04-11 16:49:24 -0600

    implement inserting new options into db and rendering them on the page

--- public/scripts/options.js
+++ public/scripts/options.js
@@ -1,15 +1,46 @@
 $(() => {
-  console.log('hi');
+
+  /**
+ * Escapes unsafe characters
+ * @param {string} str - any sequence of characters
+ * @returns same string
+ */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+/**
+ *
+ * @param {object} option
+ * @returns jquery object with html structure
+ */
+const createOption = (option) => {
+  const $option = $(`
+  <div class="option-instance">
+    <div class="option-info">
+      <div class="option-title-info">
+        <h3>${escape(option.title)}</h3>
+      </div>
+      <div class="option-description-info">
+        <p>${option.description ? escape(option.description) : ''}</p>
+      </div>
+    </div>
+    <button class="delete-option" id=${escape(option.id)}>Delete</button>
+  </div>
+  `);
+
+  return $option;
+}
+
   $('.new-option').on('submit', function (event) {
     event.preventDefault();
-    console.log('submit');
-
     const data = $(this).serialize();
-    console.log(data);
 
-
-
-
-
+    $.post('/options', data)
+    .then(response =>  createOption(response))
+    .then(element => $('.option-container').prepend(element))
   })
+
 });
--- routes/options.js
+++ routes/options.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { insertOption, getOptions } = require('../db/queries/create_option');
+
+
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  res.render('add_options.ejs', { id });
-})
+  const pollId = req.params.id;
+  getOptions(pollId)
+    .then(options => res.render('add_options.ejs', [options, pollId]))
+});
 
 router.post('/', (req, res) => {
-  console.log(req.body);
+  if (!req.body.title) {
+    // todo
+    return console.log('not ok');
+  }
+
+  const title = req.body.title;
+  const description = (!req.body.description) ? null : req.body.description;
   const pollId = req.body.pollId;
-  // add to db
-  // send back sample data
-  const id = 43;
-  const option = {title: req.body.title, description: req.body.description, id};
-  res.json(option);
-})
+
+  insertOption(pollId, title, description)
+    .then(option => res.json(option));
+});
 module.exports = router;
