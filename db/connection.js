0aad8621e6472d753ab4c8ba4591f6a0de270406
Omar Hamid <omarhamid664@gmail.com>
2023-04-11 22:33:43 -0600

    fix connections

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,12 @@
 // PG database client/connection setup
 const { Pool } = require('pg');
 
-// const dbParams = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// };
-
 const dbParams = {
-  host: 'localhost',
-  user: 'labber',
-  password: 'labber',
-  database: 'midterm'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 };
 
 const db = new Pool(dbParams);
